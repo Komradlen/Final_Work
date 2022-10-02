83cc7875e07834a9a32b2cfb7f81bc4a2bc061e3
Volkhov <111065098+Komradlen@users.noreply.github.com>
2022-10-02 16:25:51 +0300

    comm_void_1

--- Home_Work_Final/Program.cs
+++ Home_Work_Final/Program.cs
@@ -2,23 +2,40 @@
 либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
 При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами. */
 
-string[] arr_short(string[] array){
-	int length = array.Length;			
-	string[] result = new string[length];
-	int count = 0;
-			
-	for (int i = 0; i < length; i++){
-		if(array[i].Length <= lenght){
-			result[count] = array[i];
-			count++;
-			}
+void qwery(string[] array){
+	if(array.Length == 0){
+		Console.WriteLine("");
 	}
-	Array.Resize(ref result, count);
-			
-	return result;
+	else{
+		Array.ForEach(array, (str) => Console.Write($"{str} "));
+		Console.WriteLine();
+	}
+}
+
+void asdfg(string[] array){
+	Console.WriteLine("");
+	///x
+	string[] shortArray = arr_short(array);
+	Console.WriteLine("");
+	///x
+	Console.WriteLine();
 }
 
 string[] arr1 = {"Hello", "2", "world", ":-)"};
 string[] arr2 = {"1234", "1567", "-2", "computer science"};
 string[] arr3 = {"Russia", "Denmark", "Kazan"};
-int lenght = 3;
+int max_lenght = 3;
+
+string[] arr_short(string[] array){
+	int length = array.Length;			
+	string[] result = new string[length];
+	int count = 0;		
+	for (int i = 0; i < length; i++){
+		if(array[i].Length <= max_lenght){
+			result[count] = array[i];
+			count++;
+		}
+	}
+    Array.Resize(ref result, count);
+    return result;
+}
